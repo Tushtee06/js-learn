85e6c80683cf689cf960402076868f8530b784c1
Tushtee06 <114313068+Tushtee06@users.noreply.github.com>
2025-06-19 08:47:30 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello"); we are using nodejs, not browser
+
+let name = "Tushtee"
+let age = 15
+let isLoggedIn = false 
+/*
+primitive data types
+number =>  2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol => unique
+
+object
+type of null is object
+*/
+
+console.log(typeof "Tushtee");
