d1cf03e1164c5b48f3e405d604c4fe3885077fc8
Tushtee06 <114313068+Tushtee06@users.noreply.github.com>
2025-06-19 08:48:00 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "tushtee@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountId = 2 not allowed
+
+console.log(accountId);
+
+accountEmail = "ts@gmail.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
