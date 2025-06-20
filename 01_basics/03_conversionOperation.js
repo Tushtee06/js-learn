8ad6163cf7567cc2191b20f65e9b4516eeab3e5d
Tushtee06 <114313068+Tushtee06@users.noreply.github.com>
2025-06-20 12:20:21 +0000

    comparison

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = 33
+// console.log(typeof score);
+// let valueInNumber = String(score);
+// console.log(typeof valueInNumber);
+
+/*
+"33" => 33 convert in number
+"33abc" => NaN
+?? true => false
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); 
+
+// 1 => ttrue
+// 0=> false 
+// "tushtee" => true
+
+// ***************operations******************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(2%3);
+console.log(3%2);
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,6 @@
+console.log("2">1);
+console.log("02">1);
+//=== checks value and its data type
+//null>0 //null===0 no
+//null>=0 true  becaul in this case null is convert into 0 and in === case does not
+// avoid null and undefined comparisons
