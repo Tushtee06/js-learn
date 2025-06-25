c42e9e9bad8bd0f7d0df87afa8d5a76bda7ebbbd
Tushtee06 <114313068+Tushtee06@users.noreply.github.com>
2025-06-25 05:04:32 +0000

    nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,14 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+//console.log(balance.toString().length);
+
+//**maths**//
+  console.log(Math);
+  console.log(Math.abs(-4));
+  console.log(Math.round(4.6));
+console.log(Math.ceil(4.1)); // 5      
+console.log(Math.floor(4.9)); // 4
